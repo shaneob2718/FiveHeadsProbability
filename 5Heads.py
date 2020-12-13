57618a4a6c63dc2b206e2acd4e9fd29c0166efb7
Shane O'Brien <75920744+shaneob2718@users.noreply.github.com>
2020-12-12 21:29:58 -0800

    Add files via upload

--- 5Heads.py
+++ 5Heads.py
@@ -1,35 +1,46 @@
 # This program determines the probability of getting 5 heads in a row when flipping a coin
 # five times per data set for n trials, n is determined by the user
 import random
+import time
 
 flag=0
 count=0
 FiveHcount=0
 Normalcount=0
 Totalcount=0
+Timeflag=0
 
 Trialnum=int(input("How many trials would you like to run? \n"))
 
-while flag < Trialnum: # Runs the program for n (Trialnum) trials
-	ht = []
-	for i in range(0,5): # Creates the list of numbers (runs n times for n trials)
-		ht.insert(i, (str(random.choice(['H', 'T']))) )
+while Timeflag != 1:
+	t0=time.time() # Sets the initial time
 
-	print(ht) # Prints out the set of data
-	print(" ")
-	print("\n")
+	while flag < Trialnum: # Runs the program for n (Trialnum) trials
+		ht = []
+		for i in range(0,5): # Creates the list of numbers (runs n times for n trials)
+			ht.insert(i, (str(random.choice(['H', 'T']))) )
 
-	if ht[0]=='H' and ht[1]=='H' and ht[2]=='H' and ht[3]=='H' and ht[4]=='H': # Checks if a set has all heads and records it
-		FiveHcount = FiveHcount + 1
-	else: # If a set of values does not contain all heads, then the set will be counted as not having 5 heads
-		Normalcount = Normalcount + 1
+		print(ht) # Prints out the set of data
+		print(" ")
+		print("\n")
 
-	Totalcount=Totalcount+1 # Keeps track of the total amount of trials ran
-	flag=flag+1 # The exit condition for the loop, when the desired trial number n is reached, the loop will break
+		if ht[0]=='H' and ht[1]=='H' and ht[2]=='H' and ht[3]=='H' and ht[4]=='H': # Checks if a set has all heads and records it
+			FiveHcount = FiveHcount + 1
+		else: # If a set of values does not contain all heads, then the set will be counted as not having 5 heads
+			Normalcount = Normalcount + 1
+
+		Totalcount=Totalcount+1 # Keeps track of the total amount of trials ran
+		flag=flag+1 # The exit condition for the loop, when the desired trial number n is reached, the loop will break
+	Timeflag=1
+
+t_final=time.time() # Sets the final time when the loop breaks
+
+Totaltime = t_final-t0 # Calculates the total time that the simulation lasts
 
 
 print("")
 
+print("The elapsed time has been: " + str(round(Totaltime,2)) + " seconds")
 print("Total number of times five H's occurred: " + str(FiveHcount))
 print("Total number of times five H's did not occur: " + str(Normalcount))
 print("Total number of trials ran: " + str(Totalcount))
